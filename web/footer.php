800d66e0307dec2732bfb314fa6ee3eab20a8deb
Allan <wilanetoneti@gmail.com>
2016-05-19 23:48:34 -0300

    ALLAN - NOVO FOOTER

--- web/footer.php
+++ web/footer.php
@@ -1,6 +1,50 @@
 <div class="footer">
-
+	<div class="container border-footer">
+		<div class="row">
+			<div class="col-md-3">
+				<h6 class="widget_title">SOBRE</h6>
+				<ul class="list-footer">
+					<li>A pizzaria tem mais de 21 anos de tradição..</li>
+					<li><div class="social-links">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></i></a>
+							<a href="#"><i class="fa fa-google-plus"></i></a>
+							<a href="#"><i class="fa fa-instagram"></i></a>							
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<h6 class="widget_title">CÁRDAPIO</h6>
+				<ul class="list-footer">
+					<li>PIZZAS</li>
+					<li>SOBREMESSAS</li>
+					<li>BEBIDAS</li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<h6 class="widget_title">LINKS</h6>
+				<ul class="list-footer">
+					<li>MEUS PEDIDOS</li>
+					<li>MINHA CONTA</li>
+					<li>FALE CONOSCO</li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<h6 class="widget_title">INFORMAÇÕES</h6>
+				<ul class="list-footer">
+					<li>Horário de atendimento:<br/>TER A DOM - 18H A 01H</li>
+					<li>Endereço:<br/>
+					Rua da Sofrência, 11</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<p class="copyrights">© 2016 Pizzaria Big Boca — All rights reserved. Developed by Saboreweb</p>
 </div>
+
+
+
 
 
     <!-- Javascript -->

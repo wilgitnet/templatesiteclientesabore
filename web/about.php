aa893c734c9206812d99d44b8236823359db6acf
Wilson Junior <wilanetoneti@gmail.com>
2016-07-05 20:44:31 -0300

    WILSON - Mostrando mensagens de erro na busca de cep do dados de entrega quem somos dinamico

--- web/about.php
+++ web/about.php
@@ -12,10 +12,8 @@
 		<div class="about">
 			<div class="row">
 				<div class="col-md-6">
-					<h4>PIZZARIA BECO DA PIZZA</h4>
-					<p>Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gente num é. Leite de capivaris, leite de mula manquis. in elementis mé pra quem é amistosis quis leo. Copo furadis é disculpa de bebadis, arcu quam euismod magna.</p>
-					<p>Mais vale um bebadis conhecidiss, que um alcoolatra anonimiss. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Diuretics paradis num copo é motivis de denguis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.</p>
-					<p>Suco de cevadiss deixa as pessoas mais interessantiss. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.</p>
+					<h4><?PHP echo $_SESSION['nome_fantasia']; ?></h4>
+					<p><?PHP echo $_SESSION['quem_somos']; ?></p>					
 				</div>
 				<div class="col-md-6">
 					<img src="<?PHP echo $host; ?>/web/images/chef.jpg">
